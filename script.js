ab162cb0e255ed8e5d0a3b9884ee77ee494f35cd
Henrique Furtado <carloshenrique.furtado@hotmail.com>
2023-11-01 09:54:35 -0300

    Projeto base finalizado

--- script.js
+++ script.js
@@ -9,12 +9,46 @@ function addTask() {
         // Cria um item na lista 
         let li = document.createElement("li");
         // Adiciona o valor do input ao li
-        li.innerHTML = inputBox.value
+        li.innerHTML = inputBox.value;
         // Adiciona o item li ao UL
-        listContainer.appendChild(li)
+        listContainer.appendChild(li);
 
-        inputBox.value = ""
+        // Adicionando o icone X
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
+
+        inputBox.value = "";
+
+        // Salvando no Local Storage
+        saveData()
     }
 }
 
 buttonAdd.addEventListener('click', addTask)
+
+listContainer.addEventListener('click', function(e){
+    // Verifica se o elemento é um LI
+    if(e.target.tagName === "LI"){
+        // Se for um LI - Toggle na Class Checked (Altera a IMG // Coloca uma line no texto)
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN"){ 
+        // Remove o parentElement
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+
+// Salvando no Local Storage
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+    console.log(localStorage.getItem("data"))
+}
+
+showTask()
